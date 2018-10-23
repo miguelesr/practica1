4a2396ef70fd864fbd24a5c1e3f13d236a190c15
Dr. Ángel Vásquez <angel.vasquez@tecnologiasmexico.com>
2018-10-23 08:50:47 -0500

    Agregue un cuadro azul al master

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>DESARROLLO UTILIZANDO GIT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+   <div class="clase1"></div>     
+</body>
+</html>
